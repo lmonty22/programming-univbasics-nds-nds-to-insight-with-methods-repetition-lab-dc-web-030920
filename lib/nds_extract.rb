a2a287acd50154b87ffa760b5ddf8462561b0cbd
Lindsay Montgomery <lindsay.montgomery@olympian.org>
2020-03-03 17:20:36 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -3,38 +3,78 @@ require_relative './directors_database'
 
 pp directors_database
 
+# def directors_totals(source)
+#   hash = {}
+#   source.each do |director|
+#     d_name = director[:name]
+#     d_total = 0 
+#     director[:movies].each do |movie|
+#       d_total += movie[:worldwide_gross]
+#     end
+#     hash[d_name] = d_total
+#   end
+#   hash
+# end
+
 def directors_totals(source)
-  result = {}
-  director_index = 0
-  while director_index < source.size do
-    director = source[director_index]
-    result[director[:name]] = gross_for_director(director)
-    director_index += 1
+  hash = {}
+  director_index = 0 
+  while director_index < source.length do 
+    movie_index = 0 
+    d_name = source[director_index][:name]
+    d_total = 0 
+    while movie_index < source[director_index][:movies].length do 
+      d_total += source[director_index][:movies][movie_index][:worldwide_gross]
+      movie_index += 1 
+    end 
+    director_index += 1 
+    hash[d_name] = d_total
   end
-  result
+ hash 
 end
 
-def gross_for_director(d)
-  total = 0
-  index = 0
+# def gross_for_director(director_hash)
+# total = 0 
+# director_hash[:movies].each do |movie|
+#     total += movie[:worldwide_gross]
+#     end
+# total
+# end
 
-  while index < d[:movies].length do
-    total += d[:movies][index][:worldwide_gross]
-    index += 1
-  end
+def gross_for_director(director_hash)
+  total = 0 
+  movie_index = 0 
+  while movie_index < director_hash[:movies].length do 
+    total += director_hash[:movies][movie_index][:worldwide_gross]
+    movie_index += 1 
+  end 
+total 
+end 
 
-  total
-end
+
+# def list_of_directors(source)
+#   source.collect do |directors|
+#     directors[:name]
+#   end
+# end
 
 def list_of_directors(source)
-  director_index= 0
-  list_of_directors = []
-  while director_index < source.length do
-    list_of_directors.push(source[director_index][:name])
-    director_index += 1
+  array = []
+  director_index = 0 
+  while director_index < source.length do 
+    array << source[director_index][:name]
+    director_index += 1 
   end
-  list_of_directors
-end
+  array
+end 
+
+# def total_gross(source)
+# total = 0 
+# directors_totals(source).each do |key, value| 
+#   total += value
+# end 
+# total
+# end
 
 def total_gross(source)
   # Write this implementation
@@ -46,16 +86,14 @@ def total_gross(source)
   # Visit each key (i.e. director name), look up the value in the hash
   # returned by directors_totals, and add it to a running total. When done,
   # return the total
-  director_list = list_of_directors(source)
-  index = 0 
-  total = 0 
-  while index < director_list.length do
-    dtotal = directors_totals(source)
-    name = director_list[index]
-    total += dtotal[name]
-    index += 1
-  end
-  total
+total = 0 
+director_index = 0 
+while director_index < list_of_directors(source).length do 
+  total += directors_totals(source)[list_of_directors(source)[director_index]]
+  director_index += 1 
+end 
+total
 end
+  
 
 
